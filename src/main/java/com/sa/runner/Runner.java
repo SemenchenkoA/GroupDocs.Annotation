2d7ee3d2fda23f749b746a0ce3b489c29d66d5e1
SemenchenkoA <semenchenko_aleksey@mail.ru>
2022-10-24 15:04:29 +0300

    fix

--- src/main/java/com/sa/runner/Runner.java
+++ src/main/java/com/sa/runner/Runner.java
@@ -3,5 +3,10 @@ package com.sa.runner;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Main run!");
+
+        /*if(args.length > 0){
+            for(String arg :args)
+                System.out.println(arg);
+        }*/
     }
 }
